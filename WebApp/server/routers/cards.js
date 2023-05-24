bbe5fad84d111e66b567db303fc8cc0801331abe
James Leakos <jamesleakos@gmail.com>
2023-05-23 17:56:08 -0700

    quick push

--- WebApp/server/routers/cards.js
+++ WebApp/server/routers/cards.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+// internal
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = router;
