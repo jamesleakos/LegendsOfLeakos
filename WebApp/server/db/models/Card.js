d26ca8bbdeac37d757c7b99b97adfd010a96eeb2
James Leakos <jamesleakos@gmail.com>
2023-06-09 12:05:58 -0400

    quick save from car

--- WebApp/server/db/models/Card.js
+++ WebApp/server/db/models/Card.js
@@ -59,3 +59,13 @@ const CardSchema = new mongoose.Schema({
     required: false
   },
 });
+
+const PayResourceCostSchema = new mongoose.Schema({
+  statId: {
+    type: Number,
+    required: true
+  },
+  value: {
+    type: Number
+  }
+});
