496a391b70f0692e75560204a294a0adab14d537
James Leakos <jamesleakos@gmail.com>
2023-06-17 13:17:55 -0700

    quick add

---
+++ WebApp/server/db/controllers/card.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const lol = require('legends-of-leakos');
+
+const getCards = async (req, res) => {
+  try {
+    const cards = await Card.find({}).lean();
+    res.status(200).json(cards);
+  } catch (error) {
+    console.log('error: ', error);
+    res.status(500).json({ message: 'Error returning cards' });
+  }
+};
